1c4701e60b59ad3f98c3245e5d8cf1dc990d79cc
locke-123 <113772136+locke-123@users.noreply.github.com>
2023-09-12 23:09:43 +0900

    이미지 파일, 데이터 추가

--- datas/homeMenuList.ts
+++ datas/homeMenuList.ts
@@ -6,31 +6,38 @@ export const data = [
         [
             {
                 name: "강남 MICE 관광특구",
-                src: '/maincard/Gangnam MICE Special Tourist Zone.png',
+                src: '/maincard1/Gangnam MICE Special Tourist Zone.png',
+                areaCd: 'POI001'
             },
             {
                 name: "동대문 관광특구",
-                src: '/maincard/Dongdaemun Special Tourist Zone.png',
+                src: '/maincard1/Dongdaemun Special Tourist Zone.png',
+                areaCd: 'POI002'
             },
             {
                 name: "명동 관광특구",
-                src: '/livepop.png',
+                src: '/maincard1/Myeong-dong Special Tourist Zone.png',
+                areaCd: 'POI003'
             },
             {
                 name: "이태원 관광특구",
-                src: '/livepop.png',
+                src: '/maincard1/Itaewon Special Tourist Zone.png',
+                areaCd: 'POI004'
             },
             {
                 name: "잠실 관광특구",
-                src: '/livepop.png',
+                src: '/maincard1/Jamsil Special Tourist Zone.png',
+                areaCd: 'POI005'
             },
             {
                 name: "종로·청계 관광특구",
-                src: '/livepop.png',
+                src: '/maincard1/Jongno and Cheonggye Special Tourist Zone.png',
+                areaCd: 'POI006'
             },
             {
                 name: "홍대 관광특구",
-                src: '/livepop.png',
+                src: '/maincard1/Hongdae Special Tourist Zone.png',
+                areaCd: 'POI007'
             },
         ],
     },
@@ -41,23 +48,554 @@ export const data = [
         [
             {
                 name: "경복궁",
-                src: '/livepop.png',
+                src: '/maincard2/Gyeongbokgung Palace.png',
+                areaCd: 'POI008'
             },
             {
                 name: "광화문·덕수궁",
-                src: '/livepop.png',
+                src: '/maincard2/Gwanghwamun.png',
+                areaCd: 'POI009'
             },
             {
                 name: "보신각",
-                src: '/livepop.png',
+                src: '/maincard2/Bosingak.png',
+                areaCd: 'POI010'
             },
             {
                 name: "서울 암사동 유적",
-                src: '/livepop.png',
+                src: '/maincard2/Historic Site of Amsa-dong, Seoul.png',
+                areaCd: 'POI011'
             },
             {
                 name: "창덕궁·종묘",
-                src: '/livepop.png',
+                src: '/maincard2/Jongmyo Shrine, Royal Ancestral Shrine.png',
+                areaCd: 'POI012'
+            },
+        ],
+    },
+    {
+        title: "인구밀집지역",
+        type: 2,
+        list: 
+        [
+            {
+                name: "가산디지털단지역",
+                src: '1:#0253a5ff|7:#758002ff',
+                areaCd: 'POI013'
+            },
+            {
+                name: "강남역",
+                src: '2:#00a84dff|신:#d4003bff',
+                areaCd: 'POI014'
+            },
+            {
+                name: "건대입구역",
+                src: '2:#00a84dff|7:#758002ff',
+                areaCd: 'POI015'
+            },
+            {
+                name: "고덕역",
+                src: '5:#996cacff',
+                areaCd: 'POI016'
+            },
+            {
+                name: "고속터미널역",
+                src: '3:#ef7d1eff|7:#758002ff|9:#beb193ff',
+                areaCd: 'POI017'
+            },
+            {
+                name: "교대역",
+                src: '2:#00a84dff|3:#ef7d1eff',
+                areaCd: 'POI018'
+            },
+            {
+                name: "구로디지털단지역",
+                src: '2:#00a84dff',
+                areaCd: 'POI019'
+            },
+            {
+                name: "구로역",
+                src: '1:#0253a5ff',
+                areaCd: 'POI020'
+            },
+            {
+                name: "군자역",
+                src: '5:#996cacff|7:#758002ff',
+                areaCd: 'POI021'
+            },
+            {
+                name: "남구로역",
+                src: '7:#758002ff',
+                areaCd: 'POI022'
+            },
+            {
+                name: "대림역",
+                src: '2:#00a84dff|7:#758002ff',
+                areaCd: 'POI023'
+            },
+            {
+                name: "동대문역",
+                src: '1:#0253a5ff|4:#00a4e3ff',
+                areaCd: 'POI024'
+            },
+            {
+                name: "뚝섬역",
+                src: '2:#00a84dff',
+                areaCd: 'POI025'
+            },
+            {
+                name: "미아사거리역",
+                src: '4:#00a4e3ff',
+                areaCd: 'POI026'
+            },
+            {
+                name: "발산역",
+                src: '5:#996cacff',
+                areaCd: 'POI027'
+            },
+            {
+                name: "북한산우이역",
+                src: 'UI:#dbd350ff',
+                areaCd: 'POI028'
+            },
+            {
+                name: "사당역",
+                src: '2:#00a84dff|4:#00a4e3ff',
+                areaCd: 'POI029'
+            },
+            {
+                name: "삼각지역",
+                src: '4:#00a4e3ff|6:#cd7c2fff',
+                areaCd: 'POI030'
+            },
+            {
+                name: "서울대입구역",
+                src: '2:#00a84dff',
+                areaCd: 'POI031'
+            },
+            {
+                name: "서울식물원·마곡나루역",
+                src: '9:#beb193ff',
+                areaCd: 'POI032'
+            },
+            {
+                name: "서울역",
+                src: '1:#0253a5ff|4:#00a4e3ff',
+                areaCd: 'POI033'
+            },
+            {
+                name: "선릉역",
+                src: '2:#00a84dff|수:#fabf02ff',
+                areaCd: 'POI034'
+            },
+            {
+                name: "성신여대입구역",
+                src: '4:#00a4e3ff|UI:#dbd350ff',
+                areaCd: 'POI035'
+            },
+            {
+                name: "수유역",
+                src: '4:#00a4e3ff',
+                areaCd: 'POI036'
+            },
+            {
+                name: "신논현역·논현역",
+                src: '9:#beb193ff|신:#d4003bff',
+                areaCd: 'POI037'
+            },
+            {
+                name: "신도림역",
+                src: '1:#0253a5ff|2:#00a84dff',
+                areaCd: 'POI038'
+            },
+            {
+                name: "신림역",
+                src: '2:#00a84dff|신:#6789caff',
+                areaCd: 'POI039'
+            },
+            {
+                name: "신촌·이대역",
+                src: '2:#00a84dff',
+                areaCd: 'POI040'
+            },
+            {
+                name: "양재역",
+                src: '3:#ef7d1eff|신:#d4003bff',
+                areaCd: 'POI041'
+            },
+            {
+                name: "역삼역",
+                src: '2:#00a84dff',
+                areaCd: 'POI042'
+            },
+            {
+                name: "연신내역",
+                src: '3:#ef7d1eff|6:#cd7c2fff',
+                areaCd: 'POI043'
+            },
+            {
+                name: "오목교역·목동운동장",
+                src: '5:#996cacff',
+                areaCd: 'POI044'
+            },
+            {
+                name: "왕십리역",
+                src: '2:#00a84dff|5:#996cacff',
+                areaCd: 'POI045'
+            },
+            {
+                name: "용산역",
+                src: '1:#0253a5ff|경:#78c5a4ff',
+                areaCd: 'POI046'
+            },
+            {
+                name: "이태원역",
+                src: '6:#cd7c2fff',
+                areaCd: 'POI047'
+            },
+            {
+                name: "장지역",
+                src: '8:#e6186cff',
+                areaCd: 'POI048'
+            },
+            {
+                name: "장한평역",
+                src: '5:#996cacff',
+                areaCd: 'POI049'
+            },
+            {
+                name: "천호역",
+                src: '5:#996cacff|8:#e6186cff',
+                areaCd: 'POI050'
+            },
+            {
+                name: "총신대입구(이수)역",
+                src: '4:#00a4e3ff|7:#758002ff',
+                areaCd: 'POI051'
+            },
+            {
+                name: "충정로역",
+                src: '2:#00a84dff|5:#996cacff',
+                areaCd: 'POI052'
+            },
+            {
+                name: "합정역",
+                src: '2:#00a84dff|6:#cd7c2fff',
+                areaCd: 'POI053'
+            },
+            {
+                name: "혜화역",
+                src: '4:#00a4e3ff',
+                areaCd: 'POI054'
+            },
+            {
+                name: "홍대입구역 9번 출구",
+                src: '2:#00a84dff|경:#78c5a4ff',
+                areaCd: 'POI055'
+            },
+            {
+                name: "회기역",
+                src: '1:#0253a5ff|경:#78c5a4ff|경:#198d73ff',
+                areaCd: 'POI056'
+            },
+        ],
+    },
+    {
+        title: "발달상권",
+        type: 3,
+        list: 
+        [
+            {
+                name: "4·19 카페거리",
+                src: '/maincard3/419 Cafe Street.png',
+                areaCd: 'POI057'
+            },
+            {
+                name: "가락시장",
+                src: '/maincard3/Garak Market.png',
+                areaCd: 'POI058'
+            },
+            {
+                name: "가로수길",
+                src: '/maincard3/Garosu-gil Road.png',
+                areaCd: 'POI059'
+            },
+            {
+                name: "광장(전통)시장",
+                src: '/maincard3/Gwangjang Market.png',
+                areaCd: 'POI060'
+            },
+            {
+                name: "김포공항",
+                src: '/maincard3/Gimpo Airport.png',
+                areaCd: 'POI061'
+            },
+            {
+                name: "낙산공원·이화마을",
+                src: '/maincard3/Naksan Park and Yihwa Village.png',
+                areaCd: 'POI062'
+            },
+            {
+                name: "노량진",
+                src: '/maincard3/Noryangjin.png',
+                areaCd: 'POI063'
+            },
+            {
+                name: "덕수궁길·정동길",
+                src: '/maincard3/Deoksugung Road.png',
+                areaCd: 'POI064'
+            },
+            {
+                name: "방배역 먹자골목",
+                src: '/maincard3/Bangbae Food Alley.png',
+                areaCd: 'POI065'
+            },
+            {
+                name: "북촌한옥마을",
+                src: '/maincard3/Bukchon Hanok Village.png',
+                areaCd: 'POI066'
+            },
+            {
+                name: "서촌",
+                src: '/maincard3/Seochon.png',
+                areaCd: 'POI067'
+            },
+            {
+                name: "성수카페거리",
+                src: '/maincard3/Seongsu Cafe Street.png',
+                areaCd: 'POI068'
+            },
+            {
+                name: "수유리 먹자골목",
+                src: '/maincard3/Suyu-ri Food Alley.png',
+                areaCd: 'POI069'
+            },
+            {
+                name: "쌍문동 맛집거리",
+                src: '/maincard3/Ssangmun-dong Restaurant Street.png',
+                areaCd: 'POI070'
+            },
+            {
+                name: "압구정로데오거리",
+                src: '/maincard3/Apgujeong Rodeo Street.png',
+                areaCd: 'POI071'
+            },
+            {
+                name: "여의도",
+                src: '/maincard3/Yeouido.png',
+                areaCd: 'POI072'
+            },
+            {
+                name: "연남동",
+                src: '/maincard3/Yeonnam-dong.png',
+                areaCd: 'POI073'
+            },
+            {
+                name: "영등포 타임스퀘어",
+                src: '/maincard3/Yeongdeungpo Times Square.png',
+                areaCd: 'POI074'
+            },
+            {
+                name: "외대앞",
+                src: '/maincard3/in front of the Foreign Legion.png',
+                areaCd: 'POI075'
+            },
+            {
+                name: "용리단길",
+                src: '/maincard3/Yongridan-gil.png',
+                areaCd: 'POI076'
+            },
+            {
+                name: "이태원 앤틱가구거리",
+                src: '/maincard3/Itaewon Antique Furniture Street.png',
+                areaCd: 'POI077'
+            },
+            {
+                name: "인사동·익선동",
+                src: '/maincard3/Insadong.png',
+                areaCd: 'POI078'
+            },
+            {
+                name: "창동 신경제 중심지",
+                src: '/maincard3/Changdong New Economic Center.png',
+                areaCd: 'POI079'
+            },
+            {
+                name: "청담동 명품거리",
+                src: '/maincard3/Cheongdam-dong Luxury Street.png',
+                areaCd: 'POI080'
+            },
+            {
+                name: "청량리 제기동 일대 전통시장",
+                src: '/maincard3/Traditional Market in Jegi-dong, Cheongnyangni.png',
+                areaCd: 'POI081'
+            },
+            {
+                name: "해방촌·경리단길",
+                src: '/maincard3/Haebangchon Gyeongridan-gil.png',
+                areaCd: 'POI082'
+            },
+            {
+                name: "DDP(동대문디자인플라자)",
+                src: '/maincard3/DDP.png',
+                areaCd: 'POI083'
+            },
+            {
+                name: "DMC(디지털미디어시티)",
+                src: '/maincard3/DMC.png',
+                areaCd: 'POI084'
+            },
+        ],
+    },
+    {
+        title: "공원",
+        type: 3,
+        list: 
+        [
+            {
+                name: "강서한강공원",
+                src: '/maincard4/Gangseo Hangang Park.png',
+                areaCd: 'POI085'
+            },
+            {
+                name: "고척돔",
+                src: '/maincard4/Gocheok Dome.png',
+                areaCd: 'POI086'
+            },
+            {
+                name: "광나루한강공원",
+                src: '/maincard4/Gwangnaru Hangang Park.png',
+                areaCd: 'POI087'
+            },
+            {
+                name: "광화문광장",
+                src: '/maincard4/Gwanghwamun Square.png',
+                areaCd: 'POI088'
+            },
+            {
+                name: "국립중앙박물관·용산가족공원",
+                src: '/maincard4/National Museum of Korea.png',
+                areaCd: 'POI089'
+            },
+            {
+                name: "난지한강공원",
+                src: '/maincard4/Nanji Hangang Park.png',
+                areaCd: 'POI090'
+            },
+            {
+                name: "남산공원",
+                src: '/maincard4/Namsan Park.png',
+                areaCd: 'POI091'
+            },
+            {
+                name: "노들섬",
+                src: '/maincard4/Nodeul Island.png',
+                areaCd: 'POI092'
+            },
+            {
+                name: "뚝섬한강공원",
+                src: '/maincard4/Ttukseom Hangang Park.png',
+                areaCd: 'POI093'
+            },
+            {
+                name: "망원한강공원",
+                src: '/maincard4/Mangwon Hangang Park.png',
+                areaCd: 'POI094'
+            },
+            {
+                name: "반포한강공원",
+                src: '/maincard4/Banpo Hangang Park.png',
+                areaCd: 'POI095'
+            },
+            {
+                name: "북서울꿈의숲",
+                src: '/maincard4/North Seoul Dream Forest.png',
+                areaCd: 'POI096'
+            },
+            {
+                name: "불광천",
+                src: '/maincard4/a fiery spring.png',
+                areaCd: 'POI097'
+            },
+            {
+                name: "서리풀공원·몽마르뜨공원",
+                src: '/maincard4/Seoripul Park.png',
+                areaCd: 'POI098'
+            },
+            {
+                name: "서울대공원",
+                src: '/maincard4/Seoul Grand Park.png',
+                areaCd: 'POI099'
+            },
+            {
+                name: "서울숲공원",
+                src: '/maincard4/Seoul Forest Park.png',
+                areaCd: 'POI100'
+            },
+            {
+                name: "시청광장",
+                src: '/maincard4/City Hall Plaza.png',
+                areaCd: 'POI101'
+            },
+            {
+                name: "아차산",
+                src: '/maincard4/Achasan Mountain.png',
+                areaCd: 'POI102'
+            },
+            {
+                name: "양화한강공원",
+                src: '/maincard4/Yanghwa Hangang Park.png',
+                areaCd: 'POI103'
+            },
+            {
+                name: "어린이대공원",
+                src: '/maincard4/Seoul Children Grand Park.png',
+                areaCd: 'POI104'
+            },
+            {
+                name: "여의도한강공원",
+                src: '/maincard4/Yeouido Hangang Park.png',
+                areaCd: 'POI105'
+            },
+            {
+                name: "월드컵공원",
+                src: '/maincard4/World Cup Park.png',
+                areaCd: 'POI106'
+            },
+            {
+                name: "응봉산",
+                src: '/maincard4/Eungbongsan Mountain.png',
+                areaCd: 'POI107'
+            },
+            {
+                name: "이촌한강공원",
+                src: '/maincard4/Ichon Hangang Park.png',
+                areaCd: 'POI108'
+            },
+            {
+                name: "잠실종합운동장",
+                src: '/maincard4/Jamsil Sports Complex.png',
+                areaCd: 'POI109'
+            },
+            {
+                name: "잠실한강공원",
+                src: '/maincard4/Jamsil Han River Park.png',
+                areaCd: 'POI110'
+            },
+            {
+                name: "잠원한강공원",
+                src: '/maincard4/Jamwon Hangang Park.png',
+                areaCd: 'POI111'
+            },
+            {
+                name: "청계산",
+                src: '/maincard4/Cheonggye Mountain.png',
+                areaCd: 'POI112'
+            },
+            {
+                name: "청와대",
+                src: '/maincard4/The Blue House.png',
+                areaCd: 'POI113'
             },
         ],
     },
